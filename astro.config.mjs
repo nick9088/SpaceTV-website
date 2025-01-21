be532fbbaa58d7c54c28b2af0c426e3ef59ca91d
nick909 <45699480+nick9088@users.noreply.github.com>
2025-01-21 22:19:34 +0100

    hh

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,24 @@ import 'animate.css';
 import { VitePWA } from 'vite-plugin-pwa';
 import { manifest, seoConfig } from "./seoConfig.ts";
 
+import webmanifest from "astro-webmanifest";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), webmanifest()],
   vite: {
     plugins: [VitePWA({
-        registerType: "autoUpdate",
-        manifest: "./manifest.json",
-        workbox: {
-    globDirectory: "dist",
-    // globPatterns for the files you want to cache inmediately
-    // after service worker registration.
-    globPatterns: ["**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}"],
-    // Don't fallback on document based (e.g. `/some-page`) requests
-    // This removes an errant console.log message from showing up.
-    navigateFallback: null
-        }
-    })],
+      registerType: "autoUpdate",
+      manifest: "./manifest.json",
+      workbox: {
+        globDirectory: "dist",
+        // globPatterns for the files you want to cache inmediately
+        // after service worker registration.
+        globPatterns: ["**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}"],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null
+      }
+    })]
   }
 });
